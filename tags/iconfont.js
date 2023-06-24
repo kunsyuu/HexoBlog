db36ecec708999bcce2d4ab144743a89cd8239ae
kunsyuu <kunsyuu@gmail.com>
2023-06-24 23:43:08 +0800

    Site updated: 2023-06-24 23:43:07

--- tags/iconfont.js
+++ tags/iconfont.js
@@ -1,1 +1,10 @@
-"use strict";function iconFont(e){let t=(e=e.join(" ").split(","))[0],i=e[1]?e[1]:1;return`<svg class="icon" style="width:${i}em; height:${i}em" aria-hidden="true"><use xlink:href="#${t}"></use></svg>`}hexo.extend.tag.register("icon",iconFont);
+'use strict';
+
+function iconFont(args) {
+  args = args.join(' ').split(',')
+  let p0 = args[0]
+  let p1 = args[1]?args[1]:1
+  return `<svg class="icon" style="width:${p1}em; height:${p1}em" aria-hidden="true"><use xlink:href="#${p0}"></use></svg>`;
+}
+
+hexo.extend.tag.register('icon',iconFont);
